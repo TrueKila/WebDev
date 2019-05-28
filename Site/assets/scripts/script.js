8ea2c2d10632e7b343bd225750376c93e24e2392
TrueKila <ubisoft1221@mail.ru>
2019-05-29 00:32:18 +0300

    Update Files

--- Site/assets/scripts/script.js
+++ Site/assets/scripts/script.js
@@ -1,16 +1,3 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Привет Vue'
-    }
+$("#logo").click(function() {
+    $("#anim").animate({ height: "hide" }, 5000)
 });
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString(),
-        
-    }
-
-})
-app.message = 'I have changed the data!'
-app2.message = 'новое сообщение'
