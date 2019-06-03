956ec9db93118ec4da27a7a224c9b09c074602f2
TrueKila <ubisoft1221@mail.ru>
2019-06-04 02:17:17 +0300

    Update

--- Site/assets/scripts/script.js
+++ Site/assets/scripts/script.js
@@ -1,3 +1,36 @@
-$("#logo").click(function() {
-    $("#anim").animate({ height: "hide" }, 5000)
+$(function() {
+    $("div[id*='context-']").hide();
+    tab();
 });
+
+function toggle(objName) {
+    var obj = $(objName),
+        blocks = $("div[id*='context-']");
+
+    if (obj.css("display") != "none") {
+        obj.animate({ height: 'hide' }, 500);
+    } else {
+        var visibleBlocks = $("div[id*='context-']:visible");
+
+        if (visibleBlocks.length < 1) {
+            obj.animate({ height: 'show' }, 500);
+        } else {
+            $(visibleBlocks).animate({ height: 'hide' }, 500, function() {
+                obj.animate({ height: 'show' }, 500);
+            });
+        }
+    }
+}
+
+function tab() {
+
+    $('.tab-title li').not('.active').click(function() {
+        var index = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.tab-content').css('display', 'none').eq(index).css('display', 'block');
+    })
+
+    $('.tab-title li:first').addClass('active');
+    $('.tab-content:first').css('display', 'block');
+
+}
